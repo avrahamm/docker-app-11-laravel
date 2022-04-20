ddc59d3483823e0fc14441689ca3adab4dbec2a2
avrahamiq <avraham@iqdesk.com>
2022-04-20 14:12:39 +0300

    12 Running a Laravel scheduler and a queue in docker

--- docker/start.sh
+++ docker/start.sh
@@ -6,9 +6,28 @@ env=${APP_ENV:-production}
 
 #echo "The environment is $env"
 
+role=${CONTAINER_ROLE:-app}
+
+echo "The role is $role"
+
 if [ "$env" != "local" ]; then
   echo "Removing XDebug"
   rm -rf /usr/local/etc/php/conf.d/{docker-php-ext-xdebug.ini,xdebug.ini}
 fi
 
-exec apache2-foreground
+if [ "$role" = "app" ]; then
+    exec apache2-foreground
+elif [ "$role" = "scheduler" ]; then
+    while [ true ]
+    do
+        php /var/www/html/artisan schedule:run --verbose --no-interaction &
+        sleep 60
+    done
+elif [ "$role" = "queue" ]; then
+    echo "Running the queue"
+    php /var/www/html/artisan queue:work --verbose --tries=3 --timeout 90
+
+else
+    echo "Could not match the container role $role"
+    exit 1
+fi
